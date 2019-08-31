68beb4732a463ed974586f95fa6622e799be147f
George <george@Gios-MBP.home>
2019-08-30 20:26:08 -0400

    initial commit

---
+++ WheelsUpiOSTest/RootViewController/View/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheelsUpiOSTest
+//
+//  Created by Gio Lomsa on 8/28/19.
+//  Copyright © 2019 Giorgi Lomsadze. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
